ca0d1847754ec6162cc06a25336882da8fc39e17
nick.hoang@xc <nick.hoang@exelaration.com>
2021-10-14 16:09:13 -0400

    Finished two store interaction

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { useStores } from "./stores";
 
 const App = () => {
   let [value, setValue] = useState(0);
+  let [cost, setCost] = useState(0);
   let [confirmation, setConfirmation] = useState("");
+  let [costConfirmation, setCostConfirmation] = useState("");
   const {balanceStore, transactionStore} = useStores();
   return(
     useObserver(() => (
@@ -30,7 +32,7 @@ const App = () => {
       <button
         onClick={() => {
           balanceStore.withdraw(value);
-          setConfirmation(`You have successfully withrdrawn ${value} dollars.`);
+          setConfirmation(`You have successfully withdrawn ${value} dollars.`);
           setValue(0);
         }}
       >
@@ -43,6 +45,40 @@ const App = () => {
       <button
       onClick={() => {
         balanceStore.reset();
+      }}
+      >reset</button>
+
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+
+      <p>Transactions: {transactionStore.transactions}</p>
+      <input
+        id="cost"
+        type="number"
+        value={cost}
+        onChange={(event) => {
+          setCost(parseInt(event.target.value));
+        }}
+      />
+      <button
+        onClick={() => {
+          transactionStore.buySomething(cost);
+          setCostConfirmation(`You have successfully paid ${cost} dollars.`);
+          setCost(0);
+        }}
+      >Buy It!</button>
+      <h6>{costConfirmation}</h6>
+      <h2>Transactions History</h2>
+      <p>Total transactions: {transactionStore.transactions}</p>
+      <button
+      onClick={() => {
+        transactionStore.reset();
       }}
       >reset</button>
     </React.Fragment>
